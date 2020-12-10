d9c5b185c40cc9509c26a0e28896308cd2e0d7e8
USER <user@Chaeyoung.local>
2020-12-10 09:05:35 +0900

    [dev]/#31/chat state progress

--- client/src/pages/Chatpage/ChatPage.jsx
+++ client/src/pages/Chatpage/ChatPage.jsx
@@ -7,49 +7,18 @@ import MyChat from '../../components/MyChat/MyChat';
 import UserChat from '../../components/UserChat/UserChat';
 
 let roomLists = '';
-const username = '깽순이가먹다버린빵은슬펐다';
-const breedname = '블라블라블라블라블라블라블라블라블블라블라블라';
-const messageContent = '치즈모짜렐라체다고다치즈페이젝카마렐 ';
-
-const fakeData = [
-  {
-    userId: 2,
-    time: '2020-12-08 22:30',
-    text: '안녕하세요 저는 김코딩입니다.',
-  },
-  { userId: 1, time: '2020-12-08 22:31', text: '누구시죠? 안사요' },
-  {
-    userId: 2,
-    time: '2020-12-08 22:40',
-    text: '아뇨 그게 아니고요 코딩 한수를 배우기 위해서 연락을 드렸습니다.',
-  },
-  {
-    userId: 1,
-    time: '2020-12-08 22:50',
-    text:
-      '입금먼저 하시죠 제가 바로 코딩계 이구아인입니다. 한번 코딩 썻다하면 바로 스트라이크 들어갑니다.',
-  },
-  {
-    userId: 2,
-    time: '2020-12-08 23:30',
-    text: '.......?   @#$@^%^*^&( English test',
-  },
-];
-
-const testContent = fakeData.map((el) => {
-  if (el.userId === 1) {
-    return <MyChat textData={el.text} dateData={el.time} />;
-  } else {
-    return <UserChat textData={el.text} dateData={el.time} />;
-  }
-});
+let chatLists = '';
+const username = '내 이름이 나오는 곳';
+const breedname = '품종명을 쓰거나 상태메시지처럼 활용';
 
 const ChatPage = () => {
   const targetUser = createRef();
   const targetList = createRef();
 
   const [testState, setTest] = useState([]);
+  const [messageState, setMessages] = useState([]);
   const [targetId, targetToggle] = useState(-1);
+
   const testFunc = async function () {
     try {
       const res = await axios.get('http://52.78.206.149:3000/messages');
@@ -57,10 +26,20 @@ const ChatPage = () => {
     } catch (err) {
       throw err;
     } finally {
-      console.log('룸 갯 실행');
+      console.log('룸 정보 GET 함수 실행');
     }
   };
-  console.log(testState);
+
+  const messageFunc = async function () {
+    try {
+      const res = await axios.get('http://52.78.206.149:3000/messages');
+      setMessages(res.data.results);
+    } catch (err) {
+      throw err;
+    } finally {
+      console.log('메시지 정보 GET 함수 실행');
+    }
+  };
 
   if (testState.length > 0) {
     roomLists = testState.map((el) => {
@@ -76,6 +55,16 @@ const ChatPage = () => {
     });
   }
 
+  if (messageState.length > 0) {
+    chatLists = testState.map((el) => {
+      if (el.username === '28') {
+        return <MyChat textData={el.text} dateData={el.date} />;
+      } else {
+        return <UserChat textData={el.text} dateData={el.date} />;
+      }
+    });
+  }
+
   const myFunction = function (e) {
     if (e.keyCode === 13) {
       console.log(testState);
@@ -84,6 +73,7 @@ const ChatPage = () => {
 
   useEffect(() => {
     testFunc();
+    messageFunc();
   }, []);
 
   useEffect(() => {
@@ -113,38 +103,7 @@ const ChatPage = () => {
         <div className={styles.chatBox}>
           <div className={styles.chatonBoard}>
             <div className={styles.block}> </div>
-            <div className={styles.talkBox}>
-              <img
-                className={styles.chatProfile}
-                src={iguanaImg}
-                alt="chatprofile_img"
-              />
-              <div className={styles.message}>{messageContent}</div>
-              <div className={styles.dates}>2020-11-30-MON</div>
-            </div>
-            <div className={styles.talkBox}>
-              <img
-                className={styles.chatProfile}
-                src={iguanaImg}
-                alt="chatprofile_img"
-              />
-              <div className={styles.message}>{messageContent}</div>
-              <div className={styles.dates}>2020-11-30-MON</div>
-            </div>
-
-            <div className={styles.talkBox}>
-              <img
-                className={styles.chatProfile}
-                src={iguanaImg}
-                alt="chatprofile_img"
-              />
-              <div className={styles.message}>{messageContent}</div>
-              <div className={styles.dates}>2020-11-30-MON</div>
-            </div>
-            <MyChat textData={messageContent} />
-            <MyChat textData={messageContent} />
-            <MyChat textData={messageContent} />
-            {testContent}
+            {chatLists}
           </div>
           <input
             className={styles.chatPost}
