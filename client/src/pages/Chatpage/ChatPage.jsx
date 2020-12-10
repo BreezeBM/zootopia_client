f6e20ce8319238a3ef12fbc5b96d1d29f5a8daaf
USER <user@Chaeyoung.local>
2020-12-10 19:43:51 +0900

    [dev]/#31/chat state step 2

--- client/src/pages/Chatpage/ChatPage.jsx
+++ client/src/pages/Chatpage/ChatPage.jsx
@@ -1,30 +1,39 @@
 import React, { createRef, useEffect, useState } from 'react';
 import axios from 'axios';
+import io from 'socket.io-client';
 import styles from './ChatPage.module.css';
 import iguanaImg from '../../images/iguana.jpeg';
 import ChatUser from '../../components/ChatUser/ChatUser';
 import MyChat from '../../components/MyChat/MyChat';
 import UserChat from '../../components/UserChat/UserChat';
 
+const socket = io('http://e77f8aa1364b.ngrok.io', { withCredentials: true });
 let roomLists = '';
-let chatLists = '';
+// let chatLists = '';
 const username = '내 이름이 나오는 곳';
 const breedname = '품종명을 쓰거나 상태메시지처럼 활용';
 
 const ChatPage = () => {
   const targetUser = createRef();
   const targetList = createRef();
-
+  const [targetId, targetToggle] = useState('');
   const [testState, setTest] = useState([]);
-  const [messageState, setMessages] = useState([]);
-  const [targetId, targetToggle] = useState(-1);
-  const [onLine, setonLine] = useState(false);
+  const [messageState, setMessages] = useState([
+    {
+      user: '4',
+      text: '메시지 못 받음',
+      createdAt: 2020 - 12 - 23,
+    },
+  ]);
+
+  const [onLine, setonLine] = useState(0);
+  const [messageMap, setMap] = useState('');
 
   const testFunc = async function () {
     try {
       const res = await axios.get('http://e77f8aa1364b.ngrok.io/room');
-      console.log(res.data);
       setTest(res.data);
+      targetToggle(res.data[0]._id);
     } catch (err) {
       throw err;
     } finally {
@@ -32,65 +41,70 @@ const ChatPage = () => {
     }
   };
 
-  const messageFunc = async function () {
+  const messageFunc = function (id) {
     try {
-      socket.emit('joinRoom', '5fd056a52d621b1b044454a5');
-      const res2 = '';
-      const res = await axios.get('http://52.78.206.149:3000/messages');
-      setMessages(res.data.results);
+      socket.emit('joinRoom', id);
+      socket.on('renderChat', function (chat) {
+        setMessages(chat);
+      });
+      socket.on('newMessage', function (chat) {
+        setMessages(chat);
+      });
     } catch (err) {
       throw err;
     } finally {
-      console.log('메시지 정보 GET 함수 실행');
+      console.log(messageState);
     }
+    // chatLists = messageState.map((el) => {
+    //   return <UserChat textData={el.title} dateData={el.createdAt} />;
+    // });
   };
 
   if (testState.length > 0) {
     roomLists = testState.map((el) => {
+      console.log(el._id);
       return (
         <ChatUser
           idValue={el._id}
-          state={targetId}
-          stateFunc={targetToggle}
+          targetId={targetId}
+          targetToggle={targetToggle}
           roomTitle={el.title}
           roomPeople={el.people}
+          dataFunc={messageFunc}
         />
       );
     });
   }
 
-  if (messageState.length > 0) {
-    chatLists = testState.map((el) => {
-      if (el.username === '28') {
-        return <MyChat textData={el.text} dateData={el.date} />;
-      } else {
-        return <UserChat textData={el.text} dateData={el.date} />;
-      }
-    });
-  }
-
   const myFunction = function (e) {
     if (e.keyCode === 13) {
-      setonLine(true);
-      console.log(onLine);
+      const message = `{"user":"5", "text":${e.target.value}}`;
+      const config = {
+        method: 'post',
+        url: `http://e77f8aa1364b.ngrok.io/chat/${targetId}`,
+        headers: {},
+        data: message,
+      };
+      axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     }
   };
 
   useEffect(() => {
     testFunc();
-    messageFunc();
   }, []);
 
-  useEffect(() => {
-    testFunc();
-    messageFunc();
-    console.log('온라인 이펙트 실행!');
-  }, [onLine]);
+  useEffect(() => {}, [messageState]);
 
   useEffect(() => {
     const arr = ['Win16', 'Win32', 'Win64', 'Mac', 'MacIntel'];
     if (!arr.includes(navigator.platform)) {
-      if (targetId > 0) {
+      if (targetId.length > 5) {
         targetList.current.style.display = 'none';
       }
     }
@@ -114,14 +128,19 @@ const ChatPage = () => {
         <div className={styles.chatBox}>
           <div className={styles.chatonBoard}>
             <div className={styles.block}> </div>
-            {chatLists}
+            {messageState.map((el) => {
+              return <UserChat textData={el.text} dateData={el.createdAt} />;
+            })}
           </div>
           <input
             className={styles.chatPost}
             type="text"
-            placeholder="메시지 입력.."
+            placeholder="메시지 입력..."
             onKeyDown={myFunction}
           />
+          <button className={styles.send} type="button">
+            전송
+          </button>
         </div>
       </div>
     </div>
